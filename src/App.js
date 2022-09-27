d7605350db5e62453e734a7e0d4605907a00c3fa
t.mesropyan <Tatul.Mesropyan-extern@invia.de>
2022-09-28 01:37:51 +0400

    Hope working logic for posting

--- src/App.js
+++ src/App.js
@@ -13,14 +13,32 @@ function App() {
         confirmPassword: '',
     })
 
+    console.log(registrationData.username)
+
     const sendPostRequest = async () => {
+        console.log('Clicked')
         try {
-            const resp = await axios.post('localhost.6006/posts', registrationData );
-            console.log(resp.data);
-        } catch (err) {
-            // Handle Error Here
-            console.error(err);
+            await axios.post(
+                'http://localhost:8080/posts', registrationData.username, registrationData.email
+            )
         }
+        catch (err) {
+            console.error(err)
+        }
+        // try {
+        //     const { data } = await axios.post(
+        //         'https://localhost.8080/posts',
+        //         registrationData.username, Registration.email
+        //     );
+
+        //     const resp = await axios.post('localhost.8080/posts', {
+        //         method: 'POST',
+        //     });
+        //     console.log(resp.data);
+        // } catch (err) {
+        //     // Handle Error Here
+        //     console.error(err);
+        // }
     };
 
     return (
